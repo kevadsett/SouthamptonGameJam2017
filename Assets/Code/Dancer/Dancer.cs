c38689c5caf50aaac9d191139df6b3bdd84a260b
Alex Gulliver <alex.gulliver@bossalien.com>
2017-02-11 10:25:56 +0000

    Set up basic dancer which loads its limbs.

---
+++ Assets/Code/Dancer/Dancer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Dancer : MonoBehaviour
+{
+    public Transform LeftArmSocket;
+    public Transform RightArmSocket;
+    public Transform LeftLegSocket;
+    public Transform RightLegSocket;
+
+    private Limb leftArm;
+    private Limb rightArm;
+    private Limb leftLeg;
+    private Limb rightLeg;
+
+    private Limb LoadAndParentLimb(string name, Transform socket)
+    {
+        GameObject prefab = Resources.Load<GameObject>(name);
+        GameObject instance = GameObject.Instantiate(prefab);
+
+        instance.transform.SetParent(socket, false);
+
+        return instance.GetComponent<Limb>();
+    }
+
+    private void Awake()
+    {
+        leftArm = LoadAndParentLimb("Limbs/LeftArm", LeftArmSocket);
+        rightArm = LoadAndParentLimb("Limbs/RightArm", RightArmSocket);
+        leftLeg = LoadAndParentLimb("Limbs/LeftLeg", LeftLegSocket);
+        rightLeg = LoadAndParentLimb("Limbs/RightLeg", RightLegSocket);
+    }
+}
+
