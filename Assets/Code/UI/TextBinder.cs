84da47b75de5a0dd4aa7a9a57514f2fcf1136e4b
notoriousknave <notoriousknave@gmail.com>
2017-02-11 14:55:37 +0000

    Added a simple and fragile view binders system, along with an example TextBinder

---
+++ Assets/Code/UI/TextBinder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Text))]
+public class TextBinder : MonoBehaviour
+{
+	[SerializeField] private string key;
+
+	private Text text;
+
+	private void Awake ()
+	{
+		text = GetComponent<Text> ();
+	}
+
+	private void Update ()
+	{
+		string t;
+		if (ViewBindings.Instance.TryGetBoundValue (key, out t))
+		{
+			text.text = t;
+		}
+		else
+		{
+			text.text = string.Empty;
+		}
+	}
+}
