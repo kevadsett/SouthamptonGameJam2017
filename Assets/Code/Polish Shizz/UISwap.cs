ccacf7627195db09d03083759f073df83904b111
notoriousknave <notoriousknave@gmail.com>
2017-02-12 11:27:21 +0000

    FRANCE FRANCE REVOLUTION : PRESS SPACE TO START

---
+++ Assets/Code/Polish Shizz/UISwap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof (Image))]
+public class UISwap : MonoBehaviour
+{
+	public float swaprate;
+	public bool invert;
+
+	private Image image;
+
+	private void Awake ()
+	{
+		image = GetComponent<Image> ();
+	}
+
+	private void Update ()
+	{
+		float bpm = 120.0f;
+
+		float s = swaprate* (bpm / 60.0f) * Mathf.PI * 2.0f;
+		float swap = Mathf.Sin (Time.time * s);
+
+		transform.localScale = swap > 0 ^ invert ? Vector3.zero : Vector3.one;
+	}
+}
---
+++ Assets/Code/Polish Shizz/UiBob.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiBob : MonoBehaviour
+{
+	public float bobrate;
+	public float rotrate;
+	public float bobdist;
+	public float rotdist;
+	public float scarate;
+	public float scale;
+
+	private void Update ()
+	{
+		float bpm = 120.0f;
+
+		float b = bobrate * (bpm / 60.0f) * Mathf.PI * 2.0f;
+		float r = rotrate * (bpm / 60.0f) * Mathf.PI * 2.0f;
+		float s = scarate * (bpm / 60.0f) * Mathf.PI * 2.0f;
+
+		float bob = Mathf.Abs (Mathf.Sin (Time.time * b)) * bobdist;
+		float rot = Mathf.Sin (Time.time * r) * rotdist;
+		float sca = 1.0f + Mathf.Abs (Mathf.Sin (Time.time * s)) * scale;
+
+		transform.localPosition = new Vector3 (0.0f, bob, 0.0f);
+		transform.localRotation = Quaternion.Euler (0.0f, 0.0f, rot);
+		transform.localScale = Vector3.one * sca;
+	}
+}
